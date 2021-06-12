69ff5f7101c490ef814705b373bf39da192a7cfe
unknown <balochrabii7@gmail.com>
2021-06-12 19:28:28 +0500

    assinged the value and check their types

---
+++ Coding-task-1/Topic (Data Type)/Problem( CheckIt)/app.js
@@ -0,0 +1,21 @@
+//  checking the Data Types of Number
+
+let a = 38;  // this is integer value
+ console.log(typeof a);  // the typeof this nmbr is "number"
+
+
+let b = 53.5;  // this is decimal value 
+ console.log(typeof b);  // the typeof this nmbr is "number"
+
+let c = false;   // this is boolean value
+ console.log(typeof c);   // the type of this value is "boolean"
+
+ /* let d = Rabbia;   // this is string variable
+ console.log(typeof d);  // the type of this value is undefined
+ because string has no quotation mark */
+ 
+let e = "Rabbia"; // this is string value
+ console.log(typeof e);  // the type of this value is "string"
+
+let f = null;  // when we dont have any object tyo assign we write null
+ console.log(typeof f);   // the type of this value is "null"
