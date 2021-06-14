1fcc181c9788b9bc614d5b9dabf6e5d4a639b283
unknown <balochrabii7@gmail.com>
2021-06-14 23:06:55 +0500

    solve Arithmetic Operator problem

---
+++ Coding-Challenge-1/Topic-3_JS-Operator/Problem-1/app.js
@@ -0,0 +1,33 @@
+//==== solving it and output will be print
+
+var x = 5;
+var y = 10
+
+ // first condition x+y;
+let sum = x + y;
+  console.log(sum);
+
+// second condition  y-x; 
+let minus = y - x;
+  console.log(minus);
+
+// third condition  x*y;
+let multiply = x * y;
+   console.log(multiply);
+
+// fourth condition  y/x; 
+let divide = y / x;
+   console.log(divide);
+
+// fifth condition  x%2; 
+let remainder = x % 2;
+  console.log(remainder);
+
+// sixth condition  x++; 
+let increment = x++;
+   console.log(increment);
+
+// seventh condition  x--;
+let decrement = x--;
+ console.log(decrement);
+  
