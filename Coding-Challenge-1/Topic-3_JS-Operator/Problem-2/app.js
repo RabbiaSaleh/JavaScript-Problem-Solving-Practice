e4b094ee4d0ba077c73deb1a58cafd797f7174c6
unknown <balochrabii7@gmail.com>
2021-06-14 23:25:41 +0500

    solve JS Assignment operators problem

---
+++ Coding-Challenge-1/Topic-3_JS-Operator/Problem-2/app.js
@@ -0,0 +1,48 @@
+//=== solving it output will be print in console log--
+//------(their answer will be in true or False)
+
+var a = 5;
+var b = 10; 
+var c = "5";
+var x = a;
+
+// 1st condition a == c;
+let numb= a == c;
+    console.log(numb);
+
+// 2nd condition a === c;
+let numb1= a === c;
+     console.log(numb1);
+
+// 3rd condition a == x; 
+let numb2= a == x;
+    console.log(numb2);
+
+// 4th condition  a != b; 
+let numb3= a != b;
+    console.log(numb3);
+
+// 5th condition  a > b; 
+let numb4= a > b;
+    console.log(numb4);
+  
+// 6th condition  a < b;
+let numb5= a < b;
+    console.log(numb5);
+
+// 7th condition a >= b; 
+let numb6 = a >= b;
+    console.log(numb6);
+
+// 8th condition a <= b; 
+let numb7 =  a <= b;
+    console.log(numb7);
+
+// 9th condition a >= c;
+let numb8= a >= c;
+    console.log(numb8);
+
+// 10th condition a <= c;
+let numb9 = a <= c;
+    console.log(numb9);
+    
