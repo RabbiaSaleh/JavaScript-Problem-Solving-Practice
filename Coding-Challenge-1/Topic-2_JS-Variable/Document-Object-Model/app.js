6bb5097cf8553884e738b93d218908c38fdc8826
unknown <balochrabii7@gmail.com>
2021-06-14 22:42:34 +0500

    create Modeland Alert a message

--- Coding-Challenge-1/Topic-2_JS-Variable/Document-Object-Model/app.js
+++ Coding-Challenge-1/Topic-2_JS-Variable/Document-Object-Model/app.js
@@ -1,3 +1,19 @@
 function check(){
-    alert('‘The message has been sent on the given number’.');
+   //======== access input element
+    let inputElement = document.querySelector('input');
+    
+
+    //======get input element value
+    let inputValue = inputElement.value;
+   
+
+    //==== access h1 element
+    let headingElement = document.querySelector('h1 > span');
+
+    //==== change content of h1 element
+    headingElement.innerHTML= inputValue
+
+    alert('‘The message has been sent on the given number’.')
 }
+    
+
