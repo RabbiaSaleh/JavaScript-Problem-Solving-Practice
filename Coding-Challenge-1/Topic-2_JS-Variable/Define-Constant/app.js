ffa9ada7a589e156925596e982cc893b475eb739
unknown <balochrabii7@gmail.com>
2021-06-14 21:40:06 +0500

    print Variable and Constant in Console log

---
+++ Coding-Challenge-1/Topic-2_JS-Variable/Define-Constant/app.js
@@ -0,0 +1,20 @@
+//===== Creating a numeric, string, and boolean constants and printing them.
+
+
+//---number---
+ 
+const number = 35;
+  console.log(number);
+
+ //-----string---
+ 
+ const string = "my tech karo task";
+   console.log(string);
+
+//----- boolean----
+
+const  isCoding = true;
+    console.log(isCoding);
+
+const  isReading = false;
+    console.log(isReading);    
