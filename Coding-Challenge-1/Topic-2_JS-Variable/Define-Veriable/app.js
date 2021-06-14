6db7a7a38e9160139c8a85b511d5101f75ef6450
unknown <balochrabii7@gmail.com>
2021-06-14 21:26:25 +0500

    print variable in Console log

---
+++ Coding-Challenge-1/Topic-2_JS-Variable/Define-Veriable/app.js
@@ -0,0 +1,20 @@
+// =======Creating a numeric, string, and boolean variable and printing them.
+
+// ----numeric veriable
+
+let number= 3;
+    console.log(number);
+
+//---- string variable---
+
+let string = "RabbiaSaleh";
+    console.log(string);
+
+//--- boolean variable----
+
+let isGoing = true;
+   console.log(isGoing);
+
+let isLearning = false;
+   console.log(isLearning);
+     
