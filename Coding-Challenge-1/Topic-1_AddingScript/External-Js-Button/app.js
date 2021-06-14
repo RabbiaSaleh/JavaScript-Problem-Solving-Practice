2dd5e6e161e023a91c86979634e34a0b548b53e3
unknown <balochrabii7@gmail.com>
2021-06-14 19:09:35 +0500

    implement alert on button

---
+++ Coding-Challenge-1/Topic-1_AddingScript/External-Js-Button/app.js
@@ -0,0 +1,17 @@
+// ========alert for red button====
+
+function red(){
+    alert('this is red button');
+}
+
+// ========alert for blue button====
+
+function blue(){
+    alert('this is blue button');
+}
+
+// ========alert for green button====
+
+function green(){
+    alert('this is green button');
+}
