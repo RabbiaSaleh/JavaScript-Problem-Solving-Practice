a867c7082631211cf959f10373244a57577d526c
unknown <balochrabii7@gmail.com>
2021-06-21 18:05:59 +0500

    create temperature converter

---
+++ Coding-Challenge-1/Topic-3_JS-Operator/Problem-4/app.js
@@ -0,0 +1,16 @@
+/*===4. Write a program that converts degree to fahrenheit and 
+fahrenheit to degrees.
+
+F = ( 1.8 * C) + 32
+
+Where,
+
+F = Temperature in Fahrenheit
+C = Temperature in degree Celsius
+We can simply convert temperature using the above formula.
+*/
+// fisrt we are pop up prompt where user input the celsuis degree 
+ let c = prompt("put celsuis degree");
+ let f = ( 1.8 * c) + 32
+
+ document.write(f);
