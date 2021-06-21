8acb71ef3395b1e1bbc77f7f25f459b3f2f65878
unknown <balochrabii7@gmail.com>
2021-06-21 18:29:06 +0500

    campare assignment and logical operator

---
+++ Coding-Challenge-1/Topic-3_JS-Operator/Problem-3/app.js
@@ -0,0 +1,24 @@
+// ===3. Write a program that uses logical and assignment operators.
+
+// assignment operators
+let a = 3;
+let b = 8;
+    x = a * b;
+
+let c = 12;
+let d = 2;
+    y = c * d;
+
+let  w = d * b;       
+
+// logical operator its answer either be true or false
+
+z = (x == y) && (y >= x);
+    console.log(z);
+
+g = (w > x ) || (w === y);
+    console.log(g);
+
+h = (w > x ) && (w === y);
+    console.log(h);
+
