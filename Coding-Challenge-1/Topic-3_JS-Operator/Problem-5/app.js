d3f2fbcfc2fe482f4e1089444eb8f2c0f51553ea
unknown <balochrabii7@gmail.com>
2021-06-21 18:36:43 +0500

    sum two variable

---
+++ Coding-Challenge-1/Topic-3_JS-Operator/Problem-5/app.js
@@ -0,0 +1,15 @@
+// 5. Write a program that takes two variables and prints the sum of it.
+
+// first problem
+
+let a = 67;
+let b = 89;
+    sum = a + b;
+        console.log(sum); // 156 is answer and its printd in console log
+
+// 2nd problem
+
+let x = 19;
+let y = 56;
+   sum2 = x + y;
+    console.log(sum2);
