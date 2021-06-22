4ce899f48f04c60eec75b8d4dec7cc82750873b7
unknown <balochrabii7@gmail.com>
2021-06-22 20:24:21 +0500

    Practice For Loop Problem

---
+++ Coding-task-4/Loops/Infinite-loop/app.js
@@ -0,0 +1,9 @@
+/*Create a for loop.
+Use the given condition and see what happens. 
+for (;;) {}
+*/
+/* for(let i = 100; i => 0; i--){
+     console.log(i);
+}
+this is infinite loop it never stop Printing
+*/
