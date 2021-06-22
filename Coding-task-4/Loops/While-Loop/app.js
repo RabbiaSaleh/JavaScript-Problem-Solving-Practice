6e1aa375d4eafc777dc95aa8079ea42874e2c361
unknown <balochrabii7@gmail.com>
2021-06-22 19:27:51 +0500

    Print name using while Loop

---
+++ Coding-task-4/Loops/While-Loop/app.js
@@ -0,0 +1,8 @@
+/*Take a loop control variable as a.
+Initialize it.
+Using While loop, print your name 10 times on the console.*/
+var a = 1;
+while(a <= 10){
+    console.log("Rabbia Saleh");
+    a = a + 1;
+}
