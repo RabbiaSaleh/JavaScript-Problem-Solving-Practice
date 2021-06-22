2e678642e58d33db7288f5cce4c9546d4ed96544
unknown <balochrabii7@gmail.com>
2021-06-22 12:46:00 +0500

    solve assignment operator problem

---
+++ Coding-task-3/Operator/Assignment-Operator/app.js
@@ -0,0 +1,18 @@
+/*What will happen if:
+
+x=3; x+1; x+=3;
+*/
+//  the value of x = 3
+// the value of y =1
+
+let x = 3;
+let y = 1;
+let z = 3;
+
+    sum = (x+y);
+        console.log(sum);
+// the answer is 4 
+        
+   sum2 =(x += z);
+        console.log(sum2);
+// the answer is 6. in this case x = x + z and 3 + 3 is 6.        
