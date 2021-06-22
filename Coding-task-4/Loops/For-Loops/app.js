ab1a72d111035adb7e6eef1348bbfffc56a436da
unknown <balochrabii7@gmail.com>
2021-06-22 19:15:29 +0500

    Print number 0 to 200 by for Loop

---
+++ Coding-task-4/Loops/For-Loops/app.js
@@ -0,0 +1,18 @@
+/* ===1). Create a for loop. 
+    Take a loop iterating variable as i.
+    The loop should iterate less than 50 times.
+    You have to print “Clap!” on the console.
+ */
+for (let i = 0; i < 50; i++ ){
+    console.log("Clap!");
+} 
+/* 2). Create a for loop.
+
+    Take the loop iterating variable as i.
+    The loop should start from 0 and end till 200.
+    Print numbers from 0-199.
+*/
+for (let i = 0; i <= 200; i++){
+    console.log(i);
+}
+
