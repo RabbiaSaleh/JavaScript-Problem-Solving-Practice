b8ca6bdba6bee409a94dacb925b239181c31dd4e
unknown <balochrabii7@gmail.com>
2021-06-22 20:25:00 +0500

    Practice For Loop Porblems

---
+++ Coding-task-4/Loops/Do-While-Loop/app.js
@@ -0,0 +1,9 @@
+/* Take a loop control variable as a.
+Initialize it.
+Using While loop, print your name 10 times on the console in separate lines. */
+var a= 0;
+do{
+    console.log("Rabbia");
+    a = a + 1;
+}
+ while (a < 10);
