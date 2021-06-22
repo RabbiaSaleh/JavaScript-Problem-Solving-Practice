270a26e13df7216c21b7a2bc3081dd4cffec7cfd
unknown <balochrabii7@gmail.com>
2021-06-22 16:22:44 +0500

    Do task of Camparison Operator

---
+++ Coding-task-3/Operator/Camparison-Operator/app.js
@@ -0,0 +1,40 @@
+/*List down the comparison operators. 
+What happens if you write 4<=5?
+*/
+
+// === the list of camparison Operators
+// in camparison operator answer either be true or false;;;
+
+/* 1).  == equal to
+we use this symbol for equality..
+	
+2).  ===	equal value and equal 
+we use this symbol when the value and data type both are equal..
+
+3).  !=	not equal
+we use this symbol for not equality..
+
+4).  !==	not equal value or not equal 
+we use this symbol when number and data type both are not equal..	
+	
+5).  >	greater than
+we use this symbol when number is  greater then the other one..	
+
+6).  <	less than
+we use this symbol when number is  less then the other one..	
+
+7).  >=	greater than or equal to
+we use this symbol when number  is  greater then and equal to other one..	
+
+8).  <=  less than or equal to
+we use this symbol when number  is less then and equal to other one..	
+
+*/
+
+// What happens if you write 4<=5?
+
+let x = 4;
+let y = 5;
+let z = (x <= y);
+    console.log(z);
+// answer is true because x is less then y..
