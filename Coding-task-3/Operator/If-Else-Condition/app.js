81c60411e9dc7648ed2a32855f1f6324cdad865b
unknown <balochrabii7@gmail.com>
2021-06-22 13:21:05 +0500

    do if else condtions

---
+++ Coding-task-3/Operator/If-Else-Condition/app.js
@@ -0,0 +1,11 @@
+/*===Write the if else statement by using the condition that
+if student has gained marks more than or 
+equal to 50 then student is pass else student is fail.
+*/
+let marks = 60;
+if (marks => 50) {
+    document.write("student is pass");
+}
+else {
+    document.write("student is fail");
+}
