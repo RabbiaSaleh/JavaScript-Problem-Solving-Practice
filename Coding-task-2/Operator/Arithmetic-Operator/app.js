b95d624b9acfcd4c8aef371f9d64653a08a9677d
unknown <balochrabii7@gmail.com>
2021-06-22 12:25:46 +0500

    show arithmetic operator in alert

---
+++ Coding-task-2/Operator/Arithmetic-Operator/app.js
@@ -0,0 +1,22 @@
+/*===Create two variables and alert their sum, multiplication, and 
+divide var1 by var2 and see if you get the desired output or not.
+If not then see why and how to get it.
+*/
+
+var numb1 = 3;
+var numb2 = 18;
+
+//==sum
+ let sum = numb1+ numb2;
+
+ // multiplication
+  let product = numb1 * numb2;
+
+  // division
+  let divide = numb2 / numb1;
+
+  //show alert
+
+  alert(" the sum is " + sum);
+  alert("the product is " + product);
+  alert("the division is " + divide);
