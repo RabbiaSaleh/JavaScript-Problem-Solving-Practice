6d05b6e96f078b108d468ebe036090c3f59b3a86
unknown <balochrabii7@gmail.com>
2021-06-23 21:38:11 +0500

    return sum of two number in alert

---
+++ Coding-task-5/Function/Returning-Using-Another-Way/app.js
@@ -0,0 +1,8 @@
+/* Using the above example, 
+return the answer without storing it in any variable.
+*/
+
+function AddTwoNumbers(numb1 , numb2){
+    alert(numb1 + numb2);
+}
+AddTwoNumbers(67 ,87);
