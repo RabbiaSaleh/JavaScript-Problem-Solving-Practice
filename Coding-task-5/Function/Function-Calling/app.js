d09c92a3a0bf11f76c753ed64f046c7bc8ff73e0
unknown <balochrabii7@gmail.com>
2021-06-23 21:23:07 +0500

    practice function calling property

---
+++ Coding-task-5/Function/Function-Calling/app.js
@@ -0,0 +1,13 @@
+/*Using the function you have created in previous question,
+ call the function multiple times with different parameters.
+*/
+
+
+function double(paisa){
+    let answer = paisa * 2;
+    return answer;
+}
+// calling function
+console.log(double(5)); // printing answer
+double(23);
+double(67); // you can check them in console log
