a9a70b1088186e9fe4f4ab8efc8d42950b91f8f8
unknown <balochrabii7@gmail.com>
2021-06-23 13:00:42 +0500

    Printed Array one by one not using for Loop

---
+++ Coding-task-6/Array/Problem-1/app.js
@@ -0,0 +1,11 @@
+/* Initialize an array by the name of games. 
+Add any of the five games to it.
+Print this array member one by one by indexing (not using for loop).
+*/
+
+const game = ["cricket", "tennis", "golf", "soccer" ,"football"];
+    console.log(game[0]);
+    console.log(game[1]);
+    console.log(game[2]);
+    console.log(game[3]);
+    console.log(game[4]);
