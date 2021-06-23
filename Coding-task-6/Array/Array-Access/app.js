c8ef60f58c1533328c0be92f1c19408ee4c17841
unknown <balochrabii7@gmail.com>
2021-06-23 13:20:31 +0500

    Access Arrays and Print them

---
+++ Coding-task-6/Array/Array-Access/app.js
@@ -0,0 +1,7 @@
+// Print the first and last member of the same array
+
+const game = ["cricket", "tennis", "golf", "soccer" ,"football"];
+//    accessing first Element
+   console.log(game[0]);
+//    accessing last Element
+   console.log(game[4]); 
