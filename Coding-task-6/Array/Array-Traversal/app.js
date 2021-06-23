d4522f449cc350f5659a4966ba95cedc48986fd0
unknown <balochrabii7@gmail.com>
2021-06-23 13:13:55 +0500

    Change Folder Name

---
+++ Coding-task-6/Array/Array-Traversal/app.js
@@ -0,0 +1,8 @@
+/*Traverse the array using a for loop
+Print its members and index using the same for loop
+*/
+
+const game = ["cricket", "tennis", "golf", "soccer" ,"football"];
+for (let i = 0; i < game.length; i++){
+    console.log(game[i]);
+}
