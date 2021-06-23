d3ba22d34a1a4dad72b90e40a12c8389bdb2c4a0
unknown <balochrabii7@gmail.com>
2021-06-23 21:31:13 +0500

    Adding two number by function

---
+++ Coding-task-5/Function/Function-With-Multiple-Input-Parameters/app.js
@@ -0,0 +1,13 @@
+/*Create a function with the name “AddTwoNumbers.”
+Take two input parameters using names “Num1” and “Num2.”
+Perform addition operation on these input numbers.
+Save the result in variable Sum.
+Return the result.
+*/
+ function AddTwoNumbers(numb1 , numb2){
+     sum = numb1 + numb2;
+     return sum;
+ }
+ console.log(AddTwoNumbers(5 ,6));
+ // console answer ko check krne k liye kiya
+ 
