08ad87c6b1a3798627567569f9fd25a7d7c96fa3
unknown <balochrabii7@gmail.com>
2021-06-30 20:50:52 +0500

    find the last index of Loop

---
+++ Coding-task-7/Loop/Last-Index-Of/app.js
@@ -0,0 +1,7 @@
+/*
+Use the lastIndexOf function to find a ‘Cricket’ member.
+Start from the 3rd index and print its index.
+*/
+let games =  ["football", "tennis", "polo","cricket"];
+let x = games.lastIndexOf("cricket");
+    console.log(x);
