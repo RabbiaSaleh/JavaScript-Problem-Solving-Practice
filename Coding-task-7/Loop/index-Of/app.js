7196afdbf2430635969f537f4d6df6b55270a104
unknown <balochrabii7@gmail.com>
2021-06-30 20:45:41 +0500

    find the index of loop

---
+++ Coding-task-7/Loop/index-Of/app.js
@@ -0,0 +1,7 @@
+/*
+Use indexOf function to
+attempt the same problem and print index of ‘Cricket’ member
+*/
+let games =  ["cricket" ,"football", "tennis", "polo",];
+let x = games.indexOf("cricket");
+    console.log(x);
